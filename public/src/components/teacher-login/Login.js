b1f81d0b7f5f05512238517fe66ff0cd5f764907
zhangpei <13488233237@163.com>
2016-11-01 10:03:30 +0800

    add a teacher login component

--- public/src/components/teacher-login/Login.js
+++ public/src/components/teacher-login/Login.js
@@ -1,3 +1,16 @@
 /**
  * Created by zhangpei on 16/11/1.
  */
+import React, {Component} from 'react';
+
+class Login extends Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+  }
+
+}
+
+export default Login;
